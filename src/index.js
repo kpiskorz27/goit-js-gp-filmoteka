cbd0a9f4e0f55750a217ee93af671ffb58cc159e
kpiskorz27 <kpiskorz@gmx.at>
2024-03-07 20:15:21 +0100

    repo settings

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+import './js/add-to-queue';
+import './js/add-to-watched';
+import './js/modal-one-film';
+import './js/pagination';
+import './js/queue-gallery';
+import './js/watched-gallery';
