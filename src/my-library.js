e19c24a4dfc0eedc956ebe52adae63045f2984a4
kpiskorz27 <kpiskorz@gmx.at>
2024-03-11 21:08:22 +0100

    repo update

--- src/my-library.js
+++ src/my-library.js
@@ -1,7 +1,6 @@
-// import './sass/main.scss';
-import './js/film-api';
 import './js/add-to-queue';
 import './js/add-to-watched';
 import './js/modal-one-film';
 import './js/pagination';
-import './js/search-error';
+import './js/queue-gallery';
+import './js/watched-gallery';
