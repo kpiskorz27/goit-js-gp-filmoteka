e667aceded645df17a44e89ae9962568b12cefc4
kpiskorz27 <kpiskorz@gmx.at>
2024-03-12 10:56:48 +0100

    update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -46,8 +46,14 @@ export function renderMovieCard(movie) {
   movieItem.appendChild(movieTitle);
 
   const genreNames = movie.genres.map(genre => genre.name);
+  let movieGenresText = '';
+  if (genreNames.length > 2) {
+    movieGenresText = `${genreNames.slice(0, 2).join(', ')}, Other`; // Pokaż dwa pierwsze gatunki i dodaj " + other"
+  } else {
+    movieGenresText = genreNames.join(', ');
+  }
   const movieGenres = document.createElement('p');
-  movieGenres.textContent = `${genreNames.join(', ')}`;
+  movieGenres.textContent = movieGenresText;
   movieItem.appendChild(movieGenres);
 
   const movieYear = document.createElement('p');
@@ -159,60 +165,60 @@ function renderPagination(totalPages, currentPage) {
   });
 
   paginationContainer.appendChild(lastPageButton);
-
-  async function searchMovies(keyword, page = 1) {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(
-      keyword,
-    )}&page=${page}`;
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const movies = data.results;
-      const detailedMovies = await Promise.all(
-        movies.map(async movie => {
-          // Przy założeniu, że funkcja getMovieDetails jest już zdefiniowana w Twoim kodzie
-          const details = await getMovieDetails(movie.id);
-          return { ...movie, ...details };
-        }),
-      );
-      return { movies: detailedMovies, totalPages: data.total_pages };
-    } catch (error) {
-      console.error('Error while searching movies:', error);
-      return { movies: [], totalPages: 0 };
-    }
-  }
-
-  // TO SEARCH //
-  async function searchMovies(keyword, page = 1) {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(
-      keyword,
-    )}&page=${page}`;
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const movies = data.results;
-      const detailedMovies = await Promise.all(
-        movies.map(async movie => {
-          const details = await getMovieDetails(movie.id);
-          return { ...movie, ...details };
-        }),
-      );
-      return { movies: detailedMovies, totalPages: data.total_pages };
-    } catch (error) {
-      console.error('Error while searching movies:', error);
-      return { movies: [], totalPages: 0 };
-    }
-  }
-
-  async function handleSearch(keyword) {
-    const { movies, totalPages } = await searchMovies(keyword);
-    displayMovies(movies);
-    renderPagination(totalPages, 1);
-  }
-
-  document.querySelector('.search-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const keyword = document.querySelector('.search-input').value;
-    handleSearch(keyword);
-  });
 }
+
+async function searchMovies(keyword, page = 1) {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(
+    keyword,
+  )}&page=${page}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const movies = data.results;
+    const detailedMovies = await Promise.all(
+      movies.map(async movie => {
+        // Przy założeniu, że funkcja getMovieDetails jest już zdefiniowana w Twoim kodzie
+        const details = await getMovieDetails(movie.id);
+        return { ...movie, ...details };
+      }),
+    );
+    return { movies: detailedMovies, totalPages: data.total_pages };
+  } catch (error) {
+    console.error('Error while searching movies:', error);
+    return { movies: [], totalPages: 0 };
+  }
+}
+
+// TO SEARCH //
+async function searchMovies(keyword, page = 1) {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(
+    keyword,
+  )}&page=${page}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const movies = data.results;
+    const detailedMovies = await Promise.all(
+      movies.map(async movie => {
+        const details = await getMovieDetails(movie.id);
+        return { ...movie, ...details };
+      }),
+    );
+    return { movies: detailedMovies, totalPages: data.total_pages };
+  } catch (error) {
+    console.error('Error while searching movies:', error);
+    return { movies: [], totalPages: 0 };
+  }
+}
+
+async function handleSearch(keyword) {
+  const { movies, totalPages } = await searchMovies(keyword);
+  displayMovies(movies);
+  renderPagination(totalPages, 1);
+}
+
+document.querySelector('.search-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+  const keyword = document.querySelector('.search-input').value;
+  handleSearch(keyword);
+});
