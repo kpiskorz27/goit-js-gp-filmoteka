ddf0fee438b425d143ce8177b14acbcd65199eff
Eryk1599 <127441215+Eryk1599@users.noreply.github.com>
2024-03-13 21:55:00 +0100

    open/close modal
    modal support

--- src/js/modal-one-film.js
+++ src/js/modal-one-film.js
@@ -1,14 +1,38 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+import './pagination';
+const movieModal = document.querySelector('.movie-modal');
+const overlay = document.querySelector('.movie-backdrop');
+const closeBtn = document.querySelector('.modal-close-btn');
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+function openModal(event) {
+  event.preventDefault();
+  overlay.classList.remove('is-hidden');
+  movieModal.classList.remove('is-hidden');
+}
+function closeModal(event) {
+  event.preventDefault();
+  overlay.classList.add('is-hidden');
+  movieModal.classList.add('is-hidden');
+}
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+function escExit(event) {
+  const escKey = 'Escape';
+  if (event.code === escKey) {
+    closeModal(event);
   }
-})();
+}
+
+overlay.addEventListener('click', closeModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('keydown', escExit);
+
+function cardSelection() {
+  const allMovies = document.querySelectorAll('.movie-item');
+  if (allMovies.length > 0) {
+    [...allMovies].forEach(oneMovie => {
+      oneMovie.addEventListener('click', openModal);
+    });
+  } else {
+    setTimeout(cardSelection, 1000);
+  }
+}
+cardSelection();
