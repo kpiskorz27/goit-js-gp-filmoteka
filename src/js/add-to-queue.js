01e0df007b4bb900f6f56f416d7db7c74e6612ac
Estera-Mieszczak <144910099+Estera-Mieszczak@users.noreply.github.com>
2024-03-14 10:22:27 +0100

    Update add-to-queue, add-to-watched, library

--- src/js/add-to-queue.js
+++ src/js/add-to-queue.js
@@ -1,18 +1,21 @@
 const addToQueueBtn = document.querySelector(".queue-button");
 
-// let moviesOnQueue = JSON.parse(localStorage.getItem("queue")) || [];
-let moviesOnQueue = [];
+// let moviesOnQueue = [];
 const movie = {
             poster_path: "/hu40Uxp9WtpL34jv3zyWLb5zEVY.jpg",
-            genres: [{ id: 28, name: "Horror" }],
+             genres: [
+                { id: 28, name: 'Horror' },
+                { id: 27, name: 'Dramat' },
+            ],
             release_date: "2024-01-18",
             vote_average: 5.444,
-            title: "outlander",
-        }
+            title: "vjhgyugiy",
+}
+let moviesOnQueue = JSON.parse(localStorage.getItem("queue")) || movie;
 
 addToQueueBtn.addEventListener("click", () => {
         
-    moviesOnQueue.push(movie);
+    // moviesOnQueue.push(movie);
     console.log(moviesOnQueue);
     addToLibrary("queue", moviesOnQueue);
    
--- src/js/library.js
+++ src/js/library.js
@@ -1,20 +1,24 @@
 import { renderMovieCard } from "./pagination";
 import { displayMovies } from "./pagination";
 
-
-window.addEventListener('load', async() => {
-   const movie = await loadFromLibrary();
-    await renderMovieCard(movie);
-    displayMovies();
-
+let movieArray = [];
+window.addEventListener('load', async () => {
+    if (JSON.parse(localStorage.getItem("queue", "watched"))) {
+        const movie = await loadFromLibrary();
+        movieArray.push(movie);
+        console.log(movie);
+        const renderedMovie = await renderMovieCard(movie);
+        console.log(renderedMovie);
+        displayMovies(movieArray);
+    }
+    
 });
 
 function loadFromLibrary() {
     try {
             const watchedFilms = JSON.parse(localStorage.getItem("watched"));
-            const queueFilms = JSON.parse(localStorage.getItem("queue"));
-            const allFilms = watchedFilms.concat(queueFilms);
-        return allFilms;
+        const queueFilms = JSON.parse(localStorage.getItem("queue"));
+        return watchedFilms, queueFilms;
     } catch (error) {
         console.log(error);
     }
