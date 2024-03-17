bc2d649b00dfcdd72fdb9c310947a1687893e433
Bartosz Kozielski <129390881+m3riadoc@users.noreply.github.com>
2024-03-17 23:52:55 +0100

    Update pagination.js
    in progress

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -110,25 +110,39 @@ function scrollToTop() {
   window.scrollTo({ top: 0, behavior: 'smooth' }); // Funkcja przenosząca nas na góre strony (dodane w celu ulatwienia szukania filmow), Bartosz K
 }
 
+
+
+
+//Ładowanie filmow na stronie//
 async function loadMoviesPage(page) {
-  if (!currentSearchKeyword) {
-    const { movies, totalPages } = await getPopularMovies(page); //jeśli nie istnieje wyszukiwana fraza, to ładuj popularne filmy, Bartosz K
-    displayMovies(movies);
-    renderPagination(totalPages, page);
-    scrollToTop();
+  if (!currentSearchKeyword) { //jeśli nie istnieje wyszukiwana fraza
+    if (window.location.pathname === '/my-library.html') {
+      const movies = []; 
+      displayMovies(movies);
+      renderPagination(1, 1); //w my-library nie laduj popularnych filmow (nie nadpisuje wtedy filmow z watched i queue), Bartosz K
+      scrollToTop();
+    } else {
+      const { movies, totalPages } = await getPopularMovies(page); //to ładuj popularne filmy, Bartosz K
+      displayMovies(movies);
+      renderPagination(totalPages, page);
+      scrollToTop();
+    }
   } else {
-    const { movies, totalPages } = await searchMovies(currentSearchKeyword, page); // w przeciwnym razie wyszukuj filmy po wpisanej frazie
+    const { movies, totalPages } = await searchMovies(currentSearchKeyword, page); // w przeciwnym razie wyszukuj filmy po wpisanej frazie, Bartosz K
     displayMovies(movies);
     renderPagination(totalPages, page);
     scrollToTop();
   }
 }
-
 async function main() {
   loadMoviesPage(1);
 }
-
 window.addEventListener('load', main);
+
+
+
+
+
 
 // PAGINACJA //
 
@@ -240,6 +254,11 @@ export function renderPagination(totalPages, currentPage) {
 
   paginationContainer.appendChild(lastPageButton);
 }
+
+
+
+
+//WYSZUKIWANIE FILMOW//
 
 async function searchMovies(keyword, page = 1) {
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(
